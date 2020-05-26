97e3017a4a0e84063eaa3852235e5b24695d8066
davidlira1 <liradavid100@gmail.com>
2020-05-26 08:18:52 -0700

    modified gypcrete calculations

--- client/calculations/gypcrete.js
+++ client/calculations/gypcrete.js
@@ -1,8 +1,8 @@
 var gypcrete = (SF, gypThick) => {
 
-    var bags = SF * (gypThick / 12) * 1000 / 20;
-    var tons = Math.ceil(bags / 9);
-    var perFoam = SF /4;
+    var bags = Math.ceil(SF * (gypThick / 12) / 2.5);
+    var tons = Math.ceil(bags * (1.9 * 100 / 2000));
+    var perFoam = Math.ceil(SF * .31 / 450);
 
     return { bags, tons, perFoam }
 }
